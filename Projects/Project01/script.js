8d02bc833441d6231a9f26a79d5f38c8d685a046
dajah <dmedina032@west-mec.org>
2019-09-19 18:09:55 -0700

    Started project

--- Projects/Project01/script.js
+++ Projects/Project01/script.js
@@ -1,9 +1,29 @@
 /*  Project 01_11_02
 
-    Author: 
-    Date:   
+    Author: Dajah Medina
+    Date:   9-18-19 
 
     Filename: script.js
 */
 
 "use strict";
+// global varibles
+var httpRequest = false;
+var entry = "^IXIC";
+
+try {
+    httpRequest = new XMLHttpRequest();
+}
+catch (requestError) {
+    return false;
+}
+alert(httpRequest);
+return httpRequest;
+
+
+if (window.addEventListener) {
+    window.addEventListener("load", getRequestObject, false);
+} 
+else if (window.attachEvent) {
+    window.attachEvent("onload", getRequestObject);
+}
---
+++ Projects/Project_Lab/www/script.js
@@ -0,0 +1,9 @@
+/*  Project 01_11_02
+
+    Author: Dajah Medina
+    Date:   9-18-19 
+
+    Filename: script.js
+*/
+
+"use strict";
